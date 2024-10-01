8ff794139a5bc9152de6c98e36b4e043ef3f48c0
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-10-01 22:02:39 +0200

    add seo OG, Twitter card

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,47 @@ export default defineNuxtConfig({
 					content: 'Minecraft plugins, Java developer, plugin creation, Minecraft modifications, Minecraft server development, Java tutorials, young programmer, Minecraft projects'
 				},
 				{
-					name: 'og:image',
+					property: 'og:image',
+					content: '/og.webp'
+				},
+				{
+					property: 'og:url',
+					content: 'https://samvolvo.com'
+				},
+				{
+					property: 'og:type',
+					content: 'website'
+				},
+				{
+					property: 'og:title',
+					content: 'SamVolvo'
+				},
+				{
+					property: 'og:description',
+					content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail. Discover how easy it is to customize your server gameplay, boost its functionality, and attract more players. Join me for collaboration, tutorials, and sharing knowledge about programming in Java and creating innovative plugins.'
+				},
+				{
+					name: 'twitter:card',
+					content: 'summary_large_image'
+				},
+				{
+					property: 'twitter:domain',
+					content: 'samvolvo.com'
+				},
+				{
+					property: 'twitter:url',
+					content: 'https://samvolvo.com'
+				},
+				{
+					name: 'twitter:name',
+					content: 'SamVolvo'
+				},
+				{
+					name: 'twitter:description',
+					content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail. Discover how easy it is to customize your server gameplay, boost its functionality, and attract more players. Join me for collaboration, tutorials, and sharing knowledge about programming in Java and creating innovative plugins.'
+				},
+				{
+					name: 'twitter:image',
 					content: '/og.webp'
 				}
 			],
