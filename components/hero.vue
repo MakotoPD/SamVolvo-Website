e2c1624039f8230a5d413e9f9a541a7e0987e117
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-10-02 09:01:46 +0200

    add social media menu

--- components/hero.vue
+++ components/hero.vue
@@ -2,15 +2,15 @@
 	<div class="w-screen relative">
 		<NuxtImg  alt="bg" src="/img/bg/pion.png" class="-z-0 absolute right-0 bottom-0 w-[40rem] translate-y-1/2 translate-x-1/2" />
 
-		<div class="z-50 container mx-auto grid grid-cols-2">
-			<div class="flex flex-col gap-4 justify-center items-start">
+		<div class="container mx-auto grid grid-cols-2">
+			<div class="z-50 flex flex-col gap-4 justify-center items-start">
 				<div data-aos="fade-right" data-aos-delay="400"  class="flex flex-col gap-2">
 					<p class="text-text">Sam Volont</p>
 					<h1 class="text-white font-medium tracking-wider"><span class="text-primary font-['Space_Grotesk'] font-medium">Code</span> is My <br/> Creative Outlet</h1>
 				</div>
 
 				<p data-aos="fade-right" data-aos-delay="500"  class="text-text max-w-2xl">I’m a {{datediff}}-year-old Belgian developer who began coding in 2022. What started as a hobby has grown into a passion for creating software and solving problems. Since then, I’ve been developing projects and learning new technologies, turning coding into my creative outlet and ambition.</p>
-				<button data-aos="fade-right" data-aos-delay="500"  class="bg-secondary py-2 px-6 rounded-full text-light hover:bg-secondary/60 duration-150">Contact me</button>
+				<button data-aos="fade-right" data-aos-delay="500"  class="bg-secondary py-2 px-6 rounded-full text-light hover:bg-gradient-to-tr from-secondary to-primary example-4 duration-150">Contact me</button>
 			</div>
 			<div class="flex justify-end items-start">
 				<NuxtImg  alt="boy image" data-aos="fade-left" data-aos-delay="750" src="/img/boy.png" :placeholder="[15, 25, 50, 75]"  class="h-[40rem] object-contain" />
@@ -26,3 +26,39 @@
 	let dateborn = new Date(2006,1,1);
 	let datediff = datenow.getFullYear() - dateborn.getFullYear()
 </script>
+
+<style>
+
+.example-4 {
+  outline-width: 1px;
+  outline-offset: 0;
+  outline-color: rgba(0, 130, 206, 0.75);
+  outline-style: solid;
+  animation: animateOutline 2.5s ease infinite;
+}
+
+@keyframes animateOutline {
+  0% {
+    outline-width: 1px;
+    outline-offset: 0;
+    outline-color: rgba(0, 130, 206, 0);
+  }
+
+  10% {
+    outline-color: rgba(0, 130, 206, 0.75);
+  }
+
+  /* The animation finishes at 50% */
+  50% {
+    outline-width: 7px;
+    outline-offset: 4px;
+    outline-color: rgba(0, 130, 206, 0);
+  }
+
+  100% {
+    outline-width: 7px;
+    outline-offset: 4px;
+    outline-color: rgba(102, 102, 102, 0);
+  }
+}
+</style>
--- components/navbar.vue
+++ components/navbar.vue
@@ -4,14 +4,54 @@
 
 		<NuxtImg alt="SamVolvo" src="/logo/light.svg" class="z-10 h-4" data-aos="fade-right" />
 		<div data-aos="fade-left" class="text-light px-2 py-1 border border-darkborderalptha rounded-full bg-darkalptha flex items-center gap-6">
-			<a href="#BobTheMod" class="cursor-pointre font-light duration-200 hover:bg-darkborderalptha px-2 rounded-full">
+			<a href="#BobTheMod" class="cursor-pointer font-light duration-200 hover:bg-darkborderalptha px-2 rounded-full">
 				BobTheMod
 			</a>
-			<button class="cursor-pointre font-light duration-200 hover:bg-darkborderalptha px-2 rounded-full flex items-center">
+			<button 
+				@mouseover="openSmMenu" 
+				@mouseleave="closeSmMenu" 
+				class="cursor-pointre relative font-light duration-200 hover:bg-darkborderalptha px-2 rounded-full flex items-center">
 				Social Media
 				<NuxtImg title="\/" alt="\/" src="/icons/arrow.svg" class="h-6 w-6" />
+
+				<div id="smMenu" class=" absolute right-0 top-6 py-4 px-2 flex flex-col gap-2 bg-darkborder/50 rounded-xl border border-darkborder">
+					<a href="https://discord.gg/HJP69Sx2yU" target="_blank"  class="flex gap-1 cursor-pointer hover:bg-darkborder/40 duration-75 rounded-full px-2 py-0.5">
+						<NuxtImg class="h-6 cursor-pointer" src="/icons/discord.svg" title="dc" alt="discord" loading="lazy" />
+						<p class="cursor-pointer">Discord</p>
+					</a>
+					<a href="https://www.youtube.com/@SamVolvoMusic" target="_blank"  class="flex gap-1 cursor-pointer hover:bg-darkborder/40 duration-75 rounded-full px-2 py-0.5">
+						<NuxtImg class="h-6 cursor-pointer" src="/icons/youtube.svg" title="yt" alt="youtube" loading="lazy" />
+						<p class="cursor-pointer">Youtube</p>
+					</a>
+					<a href="https://www.instagram.com/SamVolont/" target="_blank"  class="flex gap-1 cursor-pointer hover:bg-darkborder/40 duration-75 rounded-full px-2 py-0.5">
+						<NuxtImg class="h-6 cursor-pointer" src="/icons/instagram.svg" title="ig" alt="instagram" loading="lazy" />
+						<p class="cursor-pointer">Instagram</p>
+					</a>
+					<a href="https://www.facebook.com/SamVolvotje/" target="_blank"  class="flex gap-1 cursor-pointer hover:bg-darkborder/40 duration-75 rounded-full px-2 py-0.5">
+						<NuxtImg class="h-6 cursor-pointer" src="/icons/facebook.svg" title="fb" alt="facebook" loading="lazy" />
+						<p class="cursor-pointer">Facebook</p> 
+					</a>
+					<a href="https://on.soundcloud.com/NkHKANd2DJSVnnSu5" target="_blank" class="w-36 flex gap-1 cursor-pointer hover:bg-darkborder/40 duration-75 rounded-full px-2 py-0.5">
+						<NuxtImg class="h-6 cursor-pointer" src="/icons/soundcloud.svg" title="soundcloud" alt="soundcloud" loading="lazy" />
+						<p class="cursor-pointer">Soundcloud</p>
+					</a>
+
+				</div>
 			</button>
 		</div>
 		
 	</div>
 </template>
+
+<script setup lang="ts">
+
+
+
+let openSmMenu = () => {
+	document.querySelector('#smMenu')?.classList.remove('hidden')
+}
+
+let closeSmMenu = () => {
+	document.querySelector('#smMenu')?.classList.add('hidden')
+}
+</script>
