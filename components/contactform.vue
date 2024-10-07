5f6e175b186094bac98cb1e472bbee11b11e9036
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2024-10-07 19:01:07 +0200

    add email service

--- components/contactform.vue
+++ components/contactform.vue
@@ -14,25 +14,36 @@
 
 				
 
-				<form class="max-w-sm mx-auto mt-8">
+				<form @submit.prevent="submitForm"  class="max-w-sm mx-auto mt-8">
 					<div class="mb-5">
 						<label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
-						<input type="email" id="email" class="cursor-text shadow-sm border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" placeholder="name@mail.com" required />
+						<input v-model="email" type="email" id="email" class="cursor-text shadow-sm border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" placeholder="name@mail.com" required />
 					</div>
 					<div class="mb-5">
 						<label for="name" class="block mb-2 text-sm font-medium text-white">Full Name</label>
-						<input type="text" id="name" class="cursor-text shadow-sm border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" placeholder="John Kowalski" required />
+						<input v-model="name" type="text" id="name" class="cursor-text shadow-sm border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" placeholder="John Kowalski" required />
 					</div>
+					
 					<div class="mb-5">
 						<label for="message" class="block mb-2 text-sm font-medium text-white">Message</label>
-						<textarea rows="4" id="message" class="cursor-text shadow-sm border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" placeholder="Message" required />
+						<textarea v-model="message" rows="4" id="message" class="cursor-text shadow-sm border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring focus:ring-blue-500 focus:border-blue-500 shadow-sm-light" placeholder="Message" required />
 					</div>
 					<div class="flex items-start mb-5">
 						<div class="flex items-center h-5">
-						<input id="terms" type="checkbox" value="" class="cursor-pointer w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+						<input v-model="terms" id="terms" type="checkbox" value="" class="cursor-pointer w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
 						</div>
 						<label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="/privacypolicy" class="text-blue-600 hover:underline dark:text-blue-500">privacy policy</a></label>
 					</div>
+
+					<NuxtTurnstile v-if="pageReady" v-model="token" :options="{theme: 'dark'}" />
+
+
+					<div>
+						<Alert v-if="sendsucces" text="Success send message!" type="success" />
+						<Alert v-if="senderror" text="Captcha is required" type="error" />
+						<Alert v-if="error" :text="error" type="error" />
+					</div>
+
 					<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
 				</form>
 
@@ -42,6 +53,71 @@
 </template>
 
 <script setup lang="ts">
-
 const emit = defineEmits(['closeclicked'])
+
+const WEB3FORMS_ACCESS_KEY = "2836bf95-f9db-4252-bdbe-6eb90cd09a87"
+
+const token = ref("");
+const pageReady = ref(false);
+
+
+onMounted(() => {
+	pageReady.value = true
+})
+
+
+const email = ref('')
+const name = ref('')
+const message = ref('')
+const terms = ref(false)
+
+const error = ref("");
+let senderror = ref(false)
+let sendsucces = ref(false)
+
+let submitForm = async () => {
+
+	if (token.value == '') {
+		setTimeout(() => {
+			error.value = 'Cpatcha not resolved, try again'
+			senderror.value = false
+		}, 8000)
+	} else {
+
+		if (email.value != "contact@samvolvo.com" && email.value != "business@samvolvo.com" && email.value != "support@samvolvo.com") {
+		
+			const response = await fetch("https://api.web3forms.com/submit", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+			body: JSON.stringify({
+				access_key: WEB3FORMS_ACCESS_KEY,
+				subject: 'Message from contact form - Website',
+				name: name.value,
+				email: email.value,
+				message: message.value,
+			}),
+			});
+			const result = await response.json();
+			if (result.success) {
+				console.log(result);
+				console.log('Message send')
+				sendsucces.value = true
+
+
+				name.value = ''
+				email.value = ''
+				message.value = ''
+				terms.value = false
+			} else if(result.error) {
+				console.log('Form Error')
+				console.log(error.value)
+				senderror.value = true
+			}
+		
+		}
+	}
+}
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,123 +1,130 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-
-  app: {
-		head: {
-			title: 'SamVolvo',
-			htmlAttrs: {
-				lang: 'en',
-			},
-			meta: [{
-					charset: 'utf-8'
-				},
-				{
-					name: 'viewport',
-					content: 'width=device-width, initial-scale=1'
-				},
-				{
-					name: 'format-detection',
-					content: 'telephone=no'
-				},
-				{
-					name: 'description',
-					content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail.'
-				},
-				{
-					name: 'keywords',
-					content: 'Minecraft plugins, Java developer, plugin creation, Minecraft modifications, Minecraft server development, Java tutorials, young programmer, Minecraft projects'
-				},
-				{
-					property: 'og:image',
-					content: '/og.webp'
-				},
-				{
-					property: 'og:url',
-					content: 'https://samvolvo.com'
-				},
-				{
-					property: 'og:type',
-					content: 'website'
-				},
-				{
-					property: 'og:title',
-					content: 'SamVolvo'
-				},
-				{
-					property: 'og:description',
-					content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail.'
-				},
-				{
-					name: 'twitter:card',
-					content: 'summary_large_image'
-				},
-				{
-					property: 'twitter:domain',
-					content: 'samvolvo.com'
-				},
-				{
-					property: 'twitter:url',
-					content: 'https://samvolvo.com'
-				},
-				{
-					name: 'twitter:name',
-					content: 'SamVolvo'
-				},
-				{
-					name: 'twitter:description',
-					content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail.'
-				},
-				{
-					name: 'twitter:image',
-					content: '/og.webp'
-				}
-			],
-			link: [{
-					rel: 'icon',
-					type: 'image/png',
-					href: '/favicon.png'
-				},
-			],
-			script: [
-				{
-					src: 'https://cloud.umami.is/script.js', defer: true, "data-website-id": '7a13869d-929f-4f48-b2a1-3a776f6e9b38'
-				},
-				{
-					src: 'https://www.googletagmanager.com/gtag/js?id=G-307K3PNGZ5', async: true,
-				},
-				{
-					children: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-307K3PNGZ5');"
-				}
-			],
-		},
-
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+  
+	app: {
+		  head: {
+			  title: 'SamVolvo',
+			  htmlAttrs: {
+				  lang: 'en',
+			  },
+			  meta: [{
+					  charset: 'utf-8'
+				  },
+				  {
+					  name: 'viewport',
+					  content: 'width=device-width, initial-scale=1'
+				  },
+				  {
+					  name: 'format-detection',
+					  content: 'telephone=no'
+				  },
+				  {
+					  name: 'description',
+					  content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail.'
+				  },
+				  {
+					  name: 'keywords',
+					  content: 'Minecraft plugins, Java developer, plugin creation, Minecraft modifications, Minecraft server development, Java tutorials, young programmer, Minecraft projects'
+				  },
+				  {
+					  property: 'og:image',
+					  content: '/og.webp'
+				  },
+				  {
+					  property: 'og:url',
+					  content: 'https://samvolvo.com'
+				  },
+				  {
+					  property: 'og:type',
+					  content: 'website'
+				  },
+				  {
+					  property: 'og:title',
+					  content: 'SamVolvo'
+				  },
+				  {
+					  property: 'og:description',
+					  content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail.'
+				  },
+				  {
+					  name: 'twitter:card',
+					  content: 'summary_large_image'
+				  },
+				  {
+					  property: 'twitter:domain',
+					  content: 'samvolvo.com'
+				  },
+				  {
+					  property: 'twitter:url',
+					  content: 'https://samvolvo.com'
+				  },
+				  {
+					  name: 'twitter:name',
+					  content: 'SamVolvo'
+				  },
+				  {
+					  name: 'twitter:description',
+					  content: 'Welcome to the site of a young Minecraft plugin developer! I offer unique solutions in Java that will enhance your gaming experience. From simple modifications to advanced plugins, each project is crafted with passion and attention to detail.'
+				  },
+				  {
+					  name: 'twitter:image',
+					  content: '/og.webp'
+				  }
+			  ],
+			  link: [{
+					  rel: 'icon',
+					  type: 'image/png',
+					  href: '/favicon.png'
+				  },
+			  ],
+			  script: [
+				  {
+					  src: 'https://cloud.umami.is/script.js', defer: true, "data-website-id": '7a13869d-929f-4f48-b2a1-3a776f6e9b38'
+				  },
+				  {
+					  src: 'https://www.googletagmanager.com/gtag/js?id=G-307K3PNGZ5', async: true,
+				  },
+				  {
+					  children: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-307K3PNGZ5');"
+				  }
+			  ],
+		  },
+  
+	  },
+  
+	modules: [
+	  '@nuxtjs/tailwindcss',
+	  '@nuxt/image',
+	  'nuxt-gtag',
+	  '@nuxtjs/google-fonts',
+	  'nuxt-aos',
+	  '@nuxt/scripts',
+	  '@nuxtjs/turnstile',
+	],
+  
+	tailwindcss: {
+	  configPath: 'tailwind.config',
 	},
-
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/image',
-    'nuxt-gtag',
-    '@nuxtjs/google-fonts',
-    'nuxt-aos',
-  ],
-
-  tailwindcss: {
-    configPath: 'tailwind.config',
-  },
-
-  googleFonts: {
-    display: 'auto',
-    families: {
-      Poppins: [ 200, 300, 400, 500, 600, 700, 800],
-      "Space Grotesk": [ 200, 300, 400, 500, 600, 700, 800],
-    }
-  },
+  
+	googleFonts: {
+	  display: 'auto',
+	  families: {
+		Poppins: [ 200, 300, 400, 500, 600, 700, 800],
+		"Space Grotesk": [ 200, 300, 400, 500, 600, 700, 800],
+	  }
+	},
+  
+  
+	css: [
+	  '@/assets/css/global.css',
+	],
 
 
-  css: [
-    '@/assets/css/global.css',
-  ],
-
-
-})
+	turnstile: {
+		siteKey: '0x4AAAAAAAw5mI3zNRUxaCyw',
+	},
+  
+  
+  })
